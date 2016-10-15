91cccdd8b9568a41588ac00ee7fa360defe75948
Antonino Calderone <antonino.calderone@gmail.com>
2016-10-15 11:57:58 +0100

    Refactoring

--- config.h
+++ config.h
@@ -41,7 +41,7 @@
 #define PACKAGE "spidyboot"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "someone@somewhere.com"
+#define PACKAGE_BUGREPORT "acaldmail@gmail.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "spidyboot"
